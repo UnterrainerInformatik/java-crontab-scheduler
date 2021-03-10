66c5f43a77fb9d4a71bd1542ce61b71033c40bc8
Psilo <psilo@unterrainer.info>
2021-03-10 09:42:40 +0100

    repair information mechanism and bump version

---
+++ src/main/java/info/unterrainer/commons/crontabscheduler/Information.java
@@ -0,0 +1,7 @@
+package info.unterrainer.commons.crontabscheduler;
+
+public class Information {
+	public static final String name = "Crontab-Scheduler";
+	public static final String buildTime = "2021-03-10T08:41:56Z";
+	public static final String pomVersion = "0.1.1";
+}
