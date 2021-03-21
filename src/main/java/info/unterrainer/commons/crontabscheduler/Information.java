6faa4adcbd828f028d23cbc7032f6bfdaaae54a8
Gerald Unterrainer <gu_falcon@hotmail.com>
2021-03-21 11:42:31 +0100

    add convenience-methods for accessing and manipulating the handler-map

--- src/main/java/info/unterrainer/commons/crontabscheduler/Information.java
+++ src/main/java/info/unterrainer/commons/crontabscheduler/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.crontabscheduler;
 
 public class Information {
 	public static final String name = "Crontab-Scheduler";
-	public static final String buildTime = "2021-03-10T08:41:56Z";
+	public static final String buildTime = "2021-03-21T10:22:28Z";
 	public static final String pomVersion = "0.1.1";
 }
